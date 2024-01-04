bc8c6c7f8170a006a058a9cd1fd94699dd1cc768
Shivank Swami <69349438+Shivankswami77@users.noreply.github.com>
2024-01-04 14:51:39 +0530

    Added Dummy Data and some react components

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
+import { getFeaturedEvents } from "../../dummy-data";
+
 export default function Home() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
       <h1>The HomePage</h1>
